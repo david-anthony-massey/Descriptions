5f0d8d999a71cbbd2fc8506ccf5e158a7a4b7dea
RoyOlivarez <royolivarez66@gmail.com>
2020-01-24 13:05:00 -0600

    First commit

---
+++ client/src/index.js
@@ -0,0 +1,6 @@
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+import App from "./components/Component1";
+import styles from "./styles/main.css";
+
+ReactDOM.render(<App />, document.getElementById("root"));
