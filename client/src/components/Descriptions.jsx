52e2601118c6f12933347e5456088a5a8c49f29d
RoyOlivarez <royolivarez66@gmail.com>
2020-02-08 15:29:02 -0600

    adding vday and compare  <a> tags

--- client/src/components/Descriptions.jsx
+++ client/src/components/Descriptions.jsx
@@ -4,11 +4,23 @@ import React from 'react';
 const Description = (props) => {
   return ( 
     <div>
+      <div className="valentines">
+        <b>
+        <font color="#008500">Arrives before Valentine's Day.</font>
+        </b> Choose delivery options in checkout.
+      </div>
       {props.description.map((description, index) => ( 
           <div key={description.id}>
-            <li>{description.productDescription}</li> 
+            <li>{description.productDescription}</li>
+            <li>{description.productDescription}</li>
+            <li>{description.productDescription}</li>
+            <li>{description.productDescription}</li>
+            <li>{description.productDescription}</li>
           </div>
-        ))}  
+        ))} 
+       <div className="compare">
+          <a href="#Compare">Compare with suimilar items</a>
+       </div> 
     </div>
   )
 }
