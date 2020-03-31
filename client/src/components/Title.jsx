108765d1e120f9d2eb1b385d1cd725944532e991
David M <d.anthony.massey@gmail.com>
2020-03-31 01:40:46 -0500

    Reconfigure for Heroku

--- client/src/components/Title.jsx
+++ client/src/components/Title.jsx
@@ -1,23 +1,27 @@
-import React from 'react';
+import React from "react";
 
-const Title = (props) => { 
+const Title = props => {
   // maps over title prop and returns the productName from the db
-  // imports Rating component 
+  // imports Rating component
 
   return (
     <div>
       <div>
-      {props.title.map((title, index) => ( 
-          <div key={title.id}>
-            <h1 id="tittle" className="a-size-large a-spacing-none" >{title.productName}</h1>
+        {props.title.map((title, index) => (
+          <div key={index}>
+            <h1 id="tittle" className="a-size-large a-spacing-none">
+              {title}
+            </h1>
           </div>
         ))}
-        </div>
-      <div className="company-line"><h6>by <a href="#Company">Company Name</a></h6></div>  
+      </div>
+      <div className="company-line">
+        <h6>
+          by <a href="#Company">Company Name</a>
+        </h6>
+      </div>
     </div>
-    
-  )
-}
+  );
+};
 
-
- export default Title;
+export default Title;
